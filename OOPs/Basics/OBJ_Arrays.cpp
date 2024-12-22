bc34e0716f5405cf5f639f1a126968d488f8fd28
Shrey K <srkkadia@gmail.com>
2024-12-22 17:47:59 +0530

    array of Objects

--- OOPs/Basics/OBJ_Arrays.cpp
+++ OOPs/Basics/OBJ_Arrays.cpp
@@ -34,14 +34,31 @@ public:
 };
 
 int main() {
-    Book book;
+    Book library[10];
 
-    book.setBookDetails(121, "ANSI C", "E. BalaguruSwamy", "Education", 400, 500);
-    // book.getBookDetails();
+    library[0].setBookDetails(12345, "The Great Gatsby", "F. Scott Fitzgerald", "Fiction", 218, 799.00);
+    library[1].setBookDetails(67890, "To Kill a Mockingbird", "Harper Lee", "Fiction", 281, 599.00);
+    library[2].setBookDetails(11121, "1984", "George Orwell", "Dystopian", 328, 650.00);
+    library[3].setBookDetails(13141, "Moby Dick", "Herman Melville", "Adventure", 585, 950.00);
+    library[4].setBookDetails(15161, "The Odyssey", "Homer", "Epic", 541, 1125.00);
+    library[5].setBookDetails(17181, "Pride and Prejudice", "Jane Austen", "Romance", 279, 699.00);
+    library[6].setBookDetails(19202, "War and Peace", "Leo Tolstoy", "Historical Fiction", 1225, 1499.00);
+    library[7].setBookDetails(21222, "The Catcher in the Rye", "J.D. Salinger", "Fiction", 277, 799.00);
+    library[8].setBookDetails(23242, "Brave New World", "Aldous Huxley", "Dystopian", 311, 725.00);
+    library[9].setBookDetails(25262, "The Lord of the Rings", "J.R.R. Tolkien", "Fantasy", 1216, 2200.00);
 
-    cout << book.toString();
+    // for (Book b : library) {
+    //     // b.getBookDetails();
+    //     // cout << "-------------------------------------------" << endl;
 
-    cout << sizeof(book);
+    //     cout << b.toString() << endl;
+    // }
+
+    for (int i = 0; i < 10; i++) {
+        library[i].getBookDetails();
+        cout << "-------------------------------------------" << endl;
+    }
+
 
     return 0;
 }
