9e919854399af5e3a356dc6992082340f28e517b
Shrey K <srkkadia@gmail.com>
2025-02-01 17:26:09 +0530

    Friend Function

---
+++ OOPs/Basics/Friend_Function.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Panth {
+private:
+    int data = 23;
+
+public:
+    friend void exploit(Panth&);
+};
+
+void exploit(Panth& obj) {
+    cout << obj.data << endl;
+}
+
+void fun() {
+    Panth p;
+    
+    exploit(p);
+
+}
+
+int main() {
+
+    fun();
+
+    return 0;
+}
