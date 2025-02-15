8cbda317fa76cb7e6bfc157a7636391a8bdc640e
Shrey K <srkkadia@gmail.com>
2025-02-15 15:02:38 +0530

    Method Overriding

---
+++ OOPs/Polymorphism/Mt_Overriding.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class Drink
+{
+public:
+    string name;
+
+    Drink(string n) : name(n) {}
+
+    virtual void serveDrink()
+    {
+        cout << "Drink: Servicing a generic drink.\n";
+    }
+};
+
+class MockTail : public Drink
+{
+public:
+    MockTail(string n) : Drink(n) {}
+
+    void serveDrink() override
+    {
+        cout << "Mocktail: Serving " << name << " in a fancy glass with fruit garnishing." << endl;
+    }
+};
+
+class ColdDrink : public Drink
+{
+public:
+    ColdDrink(string n) : Drink(n) {}
+
+    void serveDrink() override
+    {
+        cout << "ColdDrink: Serving " << name << " chilled with ice cubes." << endl;
+    }
+};
+
+int main()
+{
+    Drink* d = NULL;
+
+    d = new MockTail("Virgin Mojito");
+    d->serveDrink();
+
+    d = new ColdDrink("Pepsi");
+    d->serveDrink();
+
+    return 0;
+}
